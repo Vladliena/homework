928055912e8339345d79840cde71a5ad1625929b
vladliena <sotnikovavladlena@gmail.com>
2022-08-18 10:32:59 +0300

    homework

---
+++ js/hillelLesson6.js
@@ -0,0 +1,35 @@
+let userWord;
+let style;
+let finalString = ' '
+let numberOfTries = 3;
+
+let userChoice = confirm(`Tell me three most important words 💚`);
+
+if (userChoice) {
+    for (let i = 1; i <= numberOfTries; i++) {
+        userWord = prompt(`Word: #${i}.`)
+        userWord = userWord.replaceAll(" ", "")
+        if (userWord && isNaN(userWord)) {
+            style = prompt(`uppercase/lowercase/capitalize`)
+            userWord = userWord.replaceAll(" ", "")
+        }
+        if (!userWord || !isNaN(userWord)){
+            userWord = prompt(`Word: #${i}.`)
+            userWord = userWord.replaceAll(" ", "")
+            i--
+        }
+        if (style === `uppercase`) {
+            finalString += `${userWord} `.toUpperCase()
+        }
+        if (style === `lowercase`) {
+            finalString += `${userWord} `.toLowerCase()
+        }
+        if (style === `capitalize`) {
+            finalString += `${userWord} `.charAt(0).toUpperCase()+userWord.slice(1)
+        }
+    }
+}
+else {
+}
+
+alert(finalString)
